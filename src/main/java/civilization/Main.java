4cfe0560e0c249d1960979bf83b9b29b8e76e2cc
Bjarne <bjarnedebacker.github@gmail.com>
2026-04-14 13:13:23 +0200

    Main aangepast

--- src/main/java/civilization/Main.java
+++ src/main/java/civilization/Main.java
@@ -1,5 +1,14 @@
 package civilization;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
